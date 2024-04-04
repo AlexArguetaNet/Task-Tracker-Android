25537a3e249a5a1b575ec0ba698f8c08356fb912
Alex Argueta <alexarguetanet@gmail.com>
2024-04-04 18:23:07 -0400

    Started working on CreateTaskFragment

--- app/src/main/java/com/example/tasktrack/HomeActivity.kt
+++ app/src/main/java/com/example/tasktrack/HomeActivity.kt
@@ -1,12 +1,16 @@
 package com.example.tasktrack
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.tasktrack.databinding.ActivityHomeBinding
 import com.example.tasktrack.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -15,7 +19,7 @@ import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), CreateTaskFragment.ICreateTask {
 
     private lateinit var binding: ActivityHomeBinding;
     private lateinit var drawerNavView: NavigationView;
@@ -23,6 +27,7 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView;
     private lateinit var mAuth: FirebaseAuth;
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(this.layoutInflater);
@@ -36,6 +41,11 @@ class HomeActivity : AppCompatActivity() {
 
         // Initialize Firebase variables
         mAuth = Firebase.auth;
+
+
+        // Get user from Intent
+        val user = intent.getSerializableExtra("USER", User::class.java);
+
 
         // Set up navigation drawer
         drawerNavView = binding.navigationViewHome;
@@ -60,8 +70,31 @@ class HomeActivity : AppCompatActivity() {
             true;
         }
 
+        val drawerHeader = drawerNavView.getHeaderView(0);
+        drawerHeader.findViewById<TextView>(R.id.textViewHeaderUsername).text = user?.username;
 
 
+        // Set up bottom navigation bar
+        bottomNavigationView = binding.bottomNavigationViewHome;
+
+        bottomNavigationView.setOnItemSelectedListener {  menuItem ->
+            when(menuItem.itemId) {
+                R.id.navHome -> switchFragments(HomeFragment());
+                R.id.navCalendar -> switchFragments(CalendarFragment());
+                R.id.navNewTask -> switchFragments(CreateTaskFragment());
+            }
+            true;
+        }
+
+
+
+    }
+
+
+    private fun switchFragments(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.cardViewHome.id, fragment)
+            .commit();
     }
 
 
